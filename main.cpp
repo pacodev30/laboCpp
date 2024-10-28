84a03e0d61287e1180d876a53228db27d17da8de
pacodev30 <pacofustier@gmail.com>
2024-10-28 18:13:13 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    /*
+     *  std::out => Affichage standard (en mémoire tampon)
+     *  std::err => Erreurs (affichage direct)
+     *  std::clog => Journalisation (en mémoire tampon)
+     *  std::endl => Retour à la ligne + flush
+     *  std:flush => flush
+     */
+
+    std::cout << "Hello Esteban " << "How are you ?" << std::endl;
+    std::cout << "By";
+    return 0;
+}
