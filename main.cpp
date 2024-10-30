fad53b2121c9fea98805446e2793385d43ee7fba
pacodev30 <pacofustier@gmail.com>
2024-10-30 16:17:22 +0100

    new app

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 
+//     **
+//    ****
+//   ******
+//  ********
+// **********
+
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    int fireSize{0};
+    constexpr char SYMBOL{'*'};
+    constexpr unsigned MIN{0};
+    constexpr unsigned MAX{25};
+
+    do {
+        std::cout << "Enter a number in range [ " << MIN << "," << MAX << " ]" << std::endl;
+        std::cin >> fireSize;
+    } while (fireSize < MIN || fireSize > MAX);
+    std::cout << std::endl;
+
+    for (int i{0}; i < fireSize; i++) {
+        for (int j{0}; j < fireSize -i; j++) {
+            std::cout << " ";
+        }
+        for (int k{0}; k < 2 * i + 2; k++) {
+            std::cout << SYMBOL;
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
+
